6ebfc53271a8dbb0ee51a542299878649a0b3623
Ostap Artym <otapik125@gmail.com>
2024-07-02 19:43:30 +0300

    [Feature] Added K+Strings for storing API key

---
+++ TMDB_UIKit/Utilities/K+Strings.swift
@@ -0,0 +1,23 @@
+//
+//  K+String.swift
+//  TMDB_UIKit
+//
+//  Created by Ostap Artym on 02.07.2024.
+//
+
+import Foundation
+
+enum K {
+    
+    enum Strings: String {
+        
+        //MARK: - API -
+        
+        case api_tmdb = "bb9c069111acd5473e4758216e2ce841"
+        
+        var value: String {
+            return rawValue
+        }
+    
+    }
+}
