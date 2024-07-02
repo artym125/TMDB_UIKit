b5c54606a943ee59ea929f0f2f235c7d49b59b9f
Ostap Artym <otapik125@gmail.com>
2024-07-02 22:00:54 +0300

    [Feature] Add MoviesViewModel for handling movie data

--- TMDB_UIKit/Delegate/SceneDelegate.swift
+++ TMDB_UIKit/Delegate/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let vc = ViewController()
+        let vc = MoviesViewController()
         let navigationController = UINavigationController(rootViewController: vc)
         window.rootViewController = navigationController
         self.window = window
