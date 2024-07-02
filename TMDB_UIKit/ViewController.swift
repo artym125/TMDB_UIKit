2f39eec0d7a37ec618291efc91f61e5a8cb1a165
Ostap Artym <otapik125@gmail.com>
2024-07-02 17:50:00 +0300

    Initial Commit

---
+++ TMDB_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB_UIKit
+//
+//  Created by Ostap Artym on 02.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
